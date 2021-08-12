d43dd14f48532ce680fb19157b81ac544625d28b
abhi114 <nabhi531@gmail.com>
2021-08-12 16:37:28 +0530

    database integrated

--- app/src/main/java/com/example/call_it/LoginActivity.java
+++ app/src/main/java/com/example/call_it/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.call_it;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,21 +8,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     EditText emailBox,passwordBox;
     Button loginBtn,SignInBtn;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        auth = FirebaseAuth.getInstance();
         emailBox = findViewById(R.id.emailBox);
         passwordBox = findViewById(R.id.passwordBox);
 
         loginBtn = findViewById(R.id.Login);
         SignInBtn = findViewById(R.id.CreateButton);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email, password;
+                email = emailBox.getText().toString();
+                password = passwordBox.getText().toString();
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Enter the required Fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "Login in", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
 
         SignInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/call_it/SignUpActivity.java
+++ app/src/main/java/com/example/call_it/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.call_it;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,17 +9,26 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignUpActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
     EditText emailBox ,passwordBox,nameBox;
     Button already_account,CreateAccount;
+    FirebaseFirestore database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseFirestore.getInstance();
         setContentView(R.layout.activity_sign_up);
         emailBox = findViewById(R.id.emailBox);
         passwordBox = findViewById(R.id.passwordBox);
@@ -29,6 +39,34 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String email,password,name;
+                email = emailBox.getText().toString();
+                password = passwordBox.getText().toString();
+                name = nameBox.getText().toString();
+
+                User user = new User();
+                user.setEmail(email);
+                user.setPassword(password);
+                user.setName(name);
+
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //success
+                            database.collection("Users").document().set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Intent i = new Intent(SignUpActivity.this,LoginActivity.class);
+                                    startActivity(i);
+                                }
+                            });
+                            Toast.makeText(SignUpActivity.this, "Account is Created", Toast.LENGTH_SHORT).show();
+
+                        }else{
+                            Toast.makeText(SignUpActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
                 
             }
         });
